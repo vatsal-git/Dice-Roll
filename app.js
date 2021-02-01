2b4434b13364a2c0114d069f87ec99c1f395eaae
Vatsal Patel <69387444+vatsal-git@users.noreply.github.com>
2021-02-01 10:02:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var randomNum1 = Math.floor((Math.random() * 6) + 1);
+
+var randomDice1 = "images/dice" + randomNum1 + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomDice1);
+
+
+var randomNum2 = Math.floor((Math.random() * 6) + 1);
+
+var randomDice2 = "images/dice" + randomNum2 + ".png";
+
+document.querySelector(".img2").src = randomDice2;
+
+// TITLE
+
+if (randomNum1 > randomNum2) {
+    document.querySelector("h1").innerText = "Player 1 Wins!";
+}
+else if (randomNum1 < randomNum2) {
+    document.querySelector("h1").innerText = "Player 2 Wins!";
+}
+if (randomNum1 === randomNum2) {
+    document.querySelector("h1").innerText = "Draw!";
+}
