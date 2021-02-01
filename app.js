65d752accc57ad7774c29bec453a7a6229f80e38
Vatsal Patel <69387444+vatsal-git@users.noreply.github.com>
2021-02-01 12:24:28 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-var randomNum1 = Math.floor((Math.random() * 6) + 1);
 
-var randomDice1 = "images/dice" + randomNum1 + ".png";
+function rollDice() {
+    var diceSound = new Audio('sounds/dice.mp3');
+    diceSound.play();
 
-document.querySelector(".img1").setAttribute("src", randomDice1);
+    var randomNum1 = Math.floor((Math.random() * 6) + 1);
+    var randomDice1 = "images/dice" + randomNum1 + ".png";
+    document.querySelector(".img1").src = randomDice1;
 
+    var randomNum2 = Math.floor((Math.random() * 6) + 1);
+    var randomDice2 = "images/dice" + randomNum2 + ".png";
+    document.querySelector(".img2").src = randomDice2;
 
-var randomNum2 = Math.floor((Math.random() * 6) + 1);
-
-var randomDice2 = "images/dice" + randomNum2 + ".png";
-
-document.querySelector(".img2").src = randomDice2;
-
-// TITLE
-
-if (randomNum1 > randomNum2) {
-    document.querySelector("h1").innerText = "Player 1 Wins!";
+    if (randomNum1 > randomNum2) {
+        document.querySelector("h1").innerText = "Player 1 Wins!";
+    }
+    else if (randomNum1 < randomNum2) {
+        document.querySelector("h1").innerText = "Player 2 Wins!";
+    }
+    else if (randomNum1 === randomNum2) {
+        document.querySelector("h1").innerText = "Draw!";
+    }
 }
-else if (randomNum1 < randomNum2) {
-    document.querySelector("h1").innerText = "Player 2 Wins!";
-}
-if (randomNum1 === randomNum2) {
-    document.querySelector("h1").innerText = "Draw!";
-}
+
+document.querySelector("button").addEventListener("click", rollDice);
+
+
+
